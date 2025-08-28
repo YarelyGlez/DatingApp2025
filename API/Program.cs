e75079d05c4e8b5fd618c187b725b8d0e3d71675
YarelyGlez <112435684+YarelyGlez@users.noreply.github.com>
2025-08-28 07:45:21 -0600

    Parcial01 - Add First EF Migrations

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,14 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
+});
 
 var app = builder.Build();
 
