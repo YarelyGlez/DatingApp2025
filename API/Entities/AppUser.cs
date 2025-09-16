a3ad23b807c9454b170e76f8da3743d1ad2edffd
YarelyGlez <112435684+YarelyGlez@users.noreply.github.com>
2025-09-16 13:53:06 -0600

    Parcial02 - Add password hash salt

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,6 +7,9 @@ namespace API.Entities
         public string Id { get; set; } = Guid.NewGuid().ToString();
         public required string DisplayName { get; set; }
         public required string Email { get; set; }
+        //Entender los principios de autenticacion
+        public required byte[] PasswordHash { get; set; } //Contrasenia que guarde el usuario
+        public required byte[] PasswordSalt { get; set; } //Variacion de la contrasenia y se encripta de manera diferente
 
         
     }
