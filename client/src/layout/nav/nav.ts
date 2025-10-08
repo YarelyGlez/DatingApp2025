771fe7e0bafd516e4ddb79d09bba98e4dfcbdbb9
YarelyGlez <112435684+YarelyGlez@users.noreply.github.com>
2025-10-08 12:44:55 -0600

    Parcial02 - Add Temaplate Form in Angular

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
+  //Creamos un objeto para las credenciales 
+  protected creds: any = {};
 
+  //Hacemos un metodo login
+  login(): void {
+    console.log(this.creds);
+  }
 }
