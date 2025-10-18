c0065a7fb4dace18e342ee47199be2aca486ce97
YarelyGlez <112435684+YarelyGlez@users.noreply.github.com>
2025-10-18 09:33:47 -0600

    Parcial02 - Add routes to guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -9,10 +9,17 @@ import { authGuard } from '../core/guard/auth-guard';
 //Indicamos las rutas a donde debera de ir nuestra app
 //Debemos indicar que parte se debe recargar
 export const routes: Routes = [
-    {path: "", component: Home},//ruta vacia y principal
-    {path: "members", component: MemberList, canActivate: [authGuard]},
-    {path: "members/{id}", component: MemberDetail},
-    {path: "lists", component: Lists},
-    {path: "messages", component: Messages},
-    {path: "**", component: Home} //Cuando no hace match con ninguna
+    { path: "", component: Home },//ruta vacia y principal
+    {
+        path: "",
+        runGuardsAndResolvers: "always",
+        canActivate: [authGuard],
+        children: [
+            { path: "members", component: MemberList, canActivate: [authGuard] },
+            { path: "members/{id}", component: MemberDetail },
+            { path: "lists", component: Lists },
+            { path: "messages", component: Messages }            
+        ]
+    },
+    { path: "**", component: Home } //Cuando no hace match con ninguna
 ];
