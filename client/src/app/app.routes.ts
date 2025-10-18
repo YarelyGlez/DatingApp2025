2c469756822a11186d7e60756828d7f49780fac7
YarelyGlez <112435684+YarelyGlez@users.noreply.github.com>
2025-10-18 00:14:45 -0600

    Parcial02 - Add Routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from '../features/home/home';
+import { MemberList } from '../features/members/member-list/member-list';
+import { MemberDetail } from '../features/members/member-detail/member-detail';
+import { Lists } from '../features/lists/lists';
+import { Messages } from '../features/messages/messages';
 
-export const routes: Routes = [];
+//Indicamos las rutas a donde debera de ir nuestra app
+//Debemos indicar que parte se debe recargar
+export const routes: Routes = [
+    {path: "", component: Home},//ruta vacia y principal
+    {path: "members", component: MemberList},
+    {path: "members/{id}", component: MemberDetail},
+    {path: "lists", component: Lists},
+    {path: "messages", component: Messages},
+    {path: "**", component: Home} //Cuando no hace match con ninguna
+];
