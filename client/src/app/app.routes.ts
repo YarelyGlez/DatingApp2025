79ec32de102cf786781f90086ffdfdb1c01ecfad
YarelyGlez <112435684+YarelyGlez@users.noreply.github.com>
2025-10-24 18:45:57 -0600

    Parcial02 - Add errors

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { MemberDetail } from '../features/members/member-detail/member-detail';
 import { Lists } from '../features/lists/lists';
 import { Messages } from '../features/messages/messages';
 import { authGuard } from '../core/guard/auth-guard';
+import { TestErrors } from '../features/test-errors/test-errors';
 
 //Indicamos las rutas a donde debera de ir nuestra app
 //Debemos indicar que parte se debe recargar
@@ -21,5 +22,6 @@ export const routes: Routes = [
             { path: "messages", component: Messages }            
         ]
     },
+    { path: "errors", component: TestErrors }, //Manejo de errores 
     { path: "**", component: Home } //Cuando no hace match con ninguna
 ];
