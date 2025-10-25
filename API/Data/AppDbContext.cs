2d3449c109e0a32927b4eaf9060a9c61328ad83b
YarelyGlez <112435684+YarelyGlez@users.noreply.github.com>
2025-10-24 23:18:08 -0600

    Parcial02 - Add Member Migration

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -1,8 +1,12 @@
+using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
-    public DbSet<Entities.AppUser> Users {get; set;}
+    public DbSet<AppUser> Users { get; set; }
+    public DbSet<Member> Members { get; set; }
+    public DbSet<Photo> Photos { get; set; }
+    
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,17 +1,16 @@
-using System;
+namespace API.Entities;
 
-namespace API.Entities
+//Modela nuestra entidad de usuario en la base de datos
+public class AppUser
 {
-    //Modela nuestra entidad de usuario en la base de datos
-    public class AppUser
-    {
-        public string Id { get; set; } = Guid.NewGuid().ToString();
-        public required string DisplayName { get; set; }
-        public required string Email { get; set; }
-        //Entender los principios de autenticacion
-        public required byte[] PasswordHash { get; set; } //Contrasenia que guarde el usuario
-        public required byte[] PasswordSalt { get; set; } //Variacion de la contrasenia y se encripta de manera diferente
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public required string DisplayName { get; set; }
+    public required string Email { get; set; }
+    public string? ImageUrl { get; set; }
+    //Entender los principios de autenticacion
+    public required byte[] PasswordHash { get; set; } //Contrasenia que guarde el usuario
+    public required byte[] PasswordSalt { get; set; } //Variacion de la contrasenia y se encripta de manera diferente
 
-
-    }
+    // Navigation properties
+    public Member Member { get; set; } = null!;
 }
