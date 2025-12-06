54b6b1592abf5f61b3fc476875736dad61474cc7
YarelyGlez <112435684+YarelyGlez@users.noreply.github.com>
2025-12-06 16:36:24 -0600

    Parcial03 - Add theme manager

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 import { ToastService } from '../../core/services/toast-service';
+import { themes } from '../theme';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,7 @@ import { ToastService } from '../../core/services/toast-service';
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
-export class Nav {
+export class Nav implements OnInit{
   private router = inject(Router);
   private toast = inject(ToastService)
   //Para el login 
@@ -19,6 +20,23 @@ export class Nav {
   protected creds: any = {};
   //Para indicarle al DOM que el usuario no se ha registrado
   //protected loggedIn = signal(false);
+
+  protected selectedTheme = signal<string>(localStorage.getItem("theme") || "light");
+  protected themes = themes;
+
+  ngOnInit(): void {
+    document.documentElement.setAttribute("data-theme", this.selectedTheme());
+  }
+
+  handleSelectedTheme(theme: string) {
+    this.selectedTheme.set(theme);
+    localStorage.setItem("theme", theme);
+    document.documentElement.setAttribute("data-theme", theme);
+    const elem = document.activeElement as HTMLDivElement;
+    if (elem) {
+      elem.blur();
+    }
+  }
 
   //Hacemos un metodo login
   login(): void {
@@ -35,7 +53,7 @@ export class Nav {
     });
   }
 
-  logout(): void{
+  logout(): void {
     this.accountService.logout();
     this.router.navigateByUrl("/");
   }
